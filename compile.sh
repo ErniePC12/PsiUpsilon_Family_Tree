01caebe72463d13250ef83a28f0869be8790e25b
Matt F <mfried@mac.com>
2015-02-23 21:53:49 -0500

    I'm using the PHP through MacPorts rather than the version built in with the OS

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
-#/usr/bin/php
+#/opt/local/bin/php54
 
-php -f dny_members.php > index.html ; php -f dny_majors.php > majors.html ; php -f dny_minors.php > minors.html
+/opt/local/bin/php54 -f dny_members.php > index.html ; /opt/local/bin/php54 -f dny_majors.php > majors.html ; /opt/local/bin/php54 -f dny_minors.php > minors.html
