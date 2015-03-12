76159d78c5ce83a1569882d9378a12fd7a3bfe0a
Matt F <mfried@mac.com>
2015-03-11 23:16:01 -0400

    Changed up the menu for the site.

---
+++ menu.php
@@ -0,0 +1,5 @@
+<span class="menuLink lastUpdated"><? print 'Last Updated: '. date('F j, Y, g:i a'); ?></span>
+<span class="menuLink pageLinkClass"><a href="index.html">All members listed by Pledge Class</a></span>
+<span class="menuLink pageLinkMajor"><a href="majors.html">All members listed by Majors</a></span>
+<span class="menuLink pageLinkMinor"><a href="minors.html">All members listed by Minors</a></span>
+<span class="menuLink pageLinkSponsor"><a href="sponsors.html">All sponsors listed by Pledge Class</a></span>
