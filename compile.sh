2f8113b42446c118f34ce6fe0da52be56fcfad53
Matt F <mfried@mac.com>
2015-03-11 23:14:46 -0400

    Extra step to compile the Sponsor Page

--- compile.sh
+++ compile.sh
@@ -1,3 +1,6 @@
 #/opt/local/bin/php54
 
-/opt/local/bin/php54 -f dny_members.php > index.html ; /opt/local/bin/php54 -f dny_majors.php > majors.html ; /opt/local/bin/php54 -f dny_minors.php > minors.html
+/opt/local/bin/php54 -f dny_members.php > index.html ; 
+/opt/local/bin/php54 -f dny_majors.php > majors.html ; 
+/opt/local/bin/php54 -f dny_minors.php > minors.html ;
+/opt/local/bin/php54 -f dny_sponsors.php > sponsors.html ;
